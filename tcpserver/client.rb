7c66e4bfd8e4ce3769dc5cf44f215b49e44588ab
Tim Kharshan <timkharshan@hotmail.com>
2017-12-30 20:22:15 -0800

    more

--- tcpserver/client.rb
+++ tcpserver/client.rb
@@ -1,33 +1,41 @@
 
 require 'socket'
 require 'thread'
-
+ESCAPE_CHAR = 'q'
 server = Socket.new Socket::AF_INET, Socket::SOCK_STREAM
-server.connect Socket.pack_sockaddr_in(ARGV[0],ARGV[1])
+server.connect Socket.pack_sockaddr_in(ARGV[0] || 9876,ARGV[1] || 'localhost')
 connected = true;
-p server
+p "connected to : #{server}"
+
 server.puts("HELLO FROM CLIENT")
 
-while (connected)
-
-    read = Thread.new(server) do |client|
-        loop {
-            msg = client.gets.chomp;
-            puts msg
-        }
-    end
-
-    write = Thread.new(server) do |client|
-        loop {
-            a = gets.chomp
-            
-            if a =='q'
-                server.close
-                connected = false
-                p server
-                break
-            end
-            client.puts(a)
-        }
+write = Thread.new(server) do |client|
+    while(connected)
+        msg = gets.chomp              
+        client.puts(msg)  
+        if msg == ESCAPE_CHAR
+            connected = false
+            client.close
+        end
     end
 end
+
+read = Thread.new(server) do |client|
+    while(connected)
+        begin 
+            msg = client.gets.chomp;
+            puts "#{server} : #{msg}"
+            if msg == ESCAPE_CHAR
+                client.close
+                connected = false
+            end
+        rescue
+            puts "disconnecting + #{client}"
+        end
+    end
+end
+
+read.join
+write.join
+
+p "good bye"

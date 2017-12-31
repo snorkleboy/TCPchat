c761d045c0c563eaa338e6d6da6a6ccec42d3e87
Tim Kharshan <timkharshan@hotmail.com>
2017-12-31 00:49:13 -0800

    server console operational, users can chat

--- tcpchat/tcpserver.rb
+++ tcpchat/tcpserver.rb
@@ -7,32 +7,37 @@ ESCAPE_CHAR = 'q'
 User = Struct.new(:client, :name, :address)
 
 class Server 
-    def initialize(port, host)
+    def initialize(port, host, name = 'server')
         @users = []
         @threads = []
         @socket = Socket.new(AF_INET, SOCK_STREAM, 0)
         sockaddress = Socket.pack_sockaddr_in(port,host)
         @socket.bind(sockaddress)
+
+        @users[0]=User.new(@socket, name, 'here')
         start()
     end
 
     def start
         @socket.listen(5)
         p 'socket bound and listening'
+        start_console()
+        p 'console running'
         while(true) do
             thr = Thread.start(@socket.accept) do |connection| 
                 p "server accepted :#{connection}"
                 begin
                     user = handshake(connection)         
-                    read(user)
                 rescue
+                    p 'thread death:'
+                    p 'self'
                     Thread.Kill self
                 end
+                read(user)
             end
-            @threads.push(thr)
         end
+        @threads.push(thr)
     end
-
     def handshake(connection)
         release = false
         client = connection[0]
@@ -50,21 +55,52 @@ class Server
             end     
         end
         @users.push(user)
-        user[:client].puts "currently connected to #{@users.map{|user| user[:name]}}"
+        user[:client].puts "currently connected: #{@users.map{|user| user[:name]}}"
         return user
     end
 
     def read(user)
         loop {
             msg = user[:client].gets.chomp
-            puts "#{user[:name]}: #{msg}"
-            write_all(msg, user)
+            msg = "#{user[:name]}: #{msg}"
+            puts msg
+            begin
+                write_all(msg, user)
+            rescue
+                p 'write all error'
+            end
         }
     end
 
-    def write_all(msg, originator)
-        @users.each{|user| user[:client].puts("#{user[:name]} : #{msg}") unless user == originator}  
+    def write_all(msg, originator = null)
+        @users[1..-1].each {|user| user.client.puts(msg) unless(user == originator)}
     end
+
+    def start_console()
+        Thread.new() do
+            loop {
+                cmd = $stdin.gets.chomp
+                if (/msg*/.match(cmd))
+                    a =@users[0].name +" " + /msg (.*)/.match(cmd)[1]
+                    write_all(a,@users[0])
+                elsif(cmd == 'see')
+                    p @users
+                    puts
+                    p @threads
+                    puts 
+                    p @socket
+                elsif(cmd == 'diss')
+                    @users[1..-1].each{|user| user.client.close()}
+                end
+            }
+        end
+    end
+
+    def write_to(msg, client)
+        client.puts msg
+    end
+
+
 
 end
 

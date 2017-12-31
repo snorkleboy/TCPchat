4dcd6aece7491ea74c232c529dc8c9fba5432d60
Tim Kharshan <timkharshan@hotmail.com>
2017-12-30 16:48:19 -0800

    c style server and client working

--- tcpserver/client.rb
+++ tcpserver/client.rb
@@ -1,5 +1,9 @@
-
 require 'socket'
 
-s = Socket.new Socket::AF_INET, Socket::SOCK_STREAM
-s.connect Socket.pack_sockaddr_in(80, 'http:/www.google.com')
+s = TCPSocket.new 'localhost', 8888
+
+while line = s.gets # Read lines from socket
+  puts line         # and print them
+end
+
+s.close  
--- tcpserver/tcpserver.rb
+++ tcpserver/tcpserver.rb
@@ -1,13 +1,32 @@
 #!/usr/bin/env ruby
 
 require 'socket'
-server = TCPServer.new 8888
+include Socket::Constants
+
+socket = Socket.new(AF_INET, SOCK_STREAM, 0)
+sockaddress = Socket.pack_sockaddr_in(8888, 'localhost')
+socket.bind(sockaddress)
+listen = socket.listen(5)
+p 'socket bound and listening'
+p listen
 
 while(true) do
-    p "waiting for connection"
-    client = server.accept
-    p client
-    client.puts "in the server"
-    client.puts "time: #{Time.now}"
-    client.close
+    p "server free"
+    connection = socket.accept
+    client = connection[0]
+    p "server accepted :#{client}"
+    client.puts  "HELLO FROM SERVER"
+    
 end
+
+
+# server = TCPServer.new 8888
+
+# while(true) do
+#     p "waiting for connection"
+#     client = server.accept
+#     p client
+#     client.puts "in the server"
+#     client.puts "time: #{Time.now}"
+#     client.close
+# end
